4e9a31db88c64da1ca8de9cabecd499be880e63c
haoyujin <haoyujin2012@gmail.com>
2017-02-15 15:04:26 +0800

    终于区分清楚前台和后台views，和教材保持一致“

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'products#index'
+
   devise_for :users
+
   namespace :admin do
     resources :products
     resources :orders do
@@ -12,11 +14,13 @@ Rails.application.routes.draw do
       end
     end
   end
+
   resources :products do
     member do
       post :add_to_cart
     end
   end
+
   resources :carts do
     collection do
       delete :clean
@@ -24,8 +28,8 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'welcome#index'
   resources :cart_items
+  
   resources :orders do
     member do
       post :pay_with_alipay
@@ -33,6 +37,7 @@ Rails.application.routes.draw do
       post :apply_to_cancel
     end
   end
+
   namespace :account do
     resources :orders
   end
