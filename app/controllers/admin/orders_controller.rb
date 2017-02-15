9b661879784f5ccde8e47553bd8d1104337845a5
haoyujin <haoyujin2012@gmail.com>
2017-02-15 10:23:37 +0800

    add admin/orders/state_option button

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -18,20 +18,20 @@ class Admin::OrdersController < ApplicationController
   end
 
   def shipped
-    @order = Order.find(params[:id]
+    @order = Order.find(params[:id])
     @order.deliver!
     redirect_to :back
   end
 
   def cancel
     @order = Order.find(params[:id])
-    @order.cancell_order
+    @order.cancell_order!
     redirect_to :back
   end
 
   def return
     @order = Order.find(params[:id])
     @order.return_good!
-    redirect_to :back    
+    redirect_to :back
   end
 end
